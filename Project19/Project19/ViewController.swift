a3dd015c0392a1850868c7350776c83024298b3f
Ceren <cerengunnes@gmail.com>
2023-12-16 19:48:22 +0300

    Finished Project 19.

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Ceren Güneş on 15.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
