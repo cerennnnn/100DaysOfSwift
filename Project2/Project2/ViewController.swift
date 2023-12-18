4c529b769218fc9d0a1534f826cb31210b29bf43
Ceren <cerengunnes@gmail.com>
2023-12-18 12:50:31 +0300

    Finished Project 21.

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import NotificationCenter
 
 class ViewController: UIViewController {
     
@@ -23,6 +24,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        let register = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(registerLocal))
+        let notify = UIBarButtonItem(title: "Notify me", style: .plain, target: self, action: #selector(scheduleLocal))
+        
+        navigationItem.leftBarButtonItems = [register, notify]
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
@@ -35,6 +40,55 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion()
+    }
+    
+    @objc func registerLocal() {
+        //request permission
+        
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh!")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        registerCategories()
+        
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Hey, come back!"
+        content.body = "We missed you! Come play!"
+        content.sound = .default
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "get back here pls"]
+        
+        var dateComponents = DateComponents()
+        dateComponents.weekday = 1
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        //        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more", options: .foreground)
+        let action = UNNotificationAction(identifier: "alarm", title: "Remind me later", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
     }
     
     @objc func askQuestion(action: UIAlertAction! = nil) {
@@ -102,3 +156,24 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default identifier.")
+            case "show":
+                    print("Show more information...")
+            default:
+                break
+            }
+        }
+        
+        completionHandler()
+    }
+}
